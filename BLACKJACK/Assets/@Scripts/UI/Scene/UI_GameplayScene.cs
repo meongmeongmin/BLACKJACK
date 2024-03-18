cf9e10f4ba5ba155e9ffd1c00f38b3b2d75db822
Meongmeongmin <pr620718@cu.ac.kr>
2024-03-18 10:18:18 +0900

    🚀 Feat: Play 버튼 로직 강화 및 UI 최적화
    - OnClickPlayButton 메서드에 게임 플레이 로직 시작 조건 추가
    - PlayerInfo의 CardDecks에 따라 각 카드 덱의 상태(배팅 여부)에 기반한 UI 요소(토글, 배팅 텍스트, 점수 텍스트) 활성화/비활성화 로직 최적화
    - 불필요한 ScoreImage와 BetImage 참조 제거 및 ScoreText와 BetText의 부모 오브젝트 활성화로 변경하여 코드 간결화

--- BLACKJACK/Assets/@Scripts/UI/Scene/UI_GameplayScene.cs
+++ BLACKJACK/Assets/@Scripts/UI/Scene/UI_GameplayScene.cs
@@ -7,7 +7,6 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static Define;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class UI_GameplayScene : UI_Scene
 {
@@ -67,11 +66,6 @@ public class UI_GameplayScene : UI_Scene
         SelectCardDeckIconImage_3,
         SelectCardDeckIconImage_4,
         SelectCardDeckIconImage_5,
-        ScoreImage_1,
-        ScoreImage_2,
-        ScoreImage_3,
-        ScoreImage_4,
-        ScoreImage_5,
     }
     #endregion
 
@@ -121,11 +115,10 @@ public class UI_GameplayScene : UI_Scene
         _player.SelectCardDeck(0);
         GetObejct((int)GameObjects.SetBetting).gameObject.SetActive(true);
         GetObejct((int)GameObjects.SetPlaying).gameObject.SetActive(false);
-        GetImage((int)Images.ScoreImage_1).gameObject.SetActive(false);
-        GetImage((int)Images.ScoreImage_2).gameObject.SetActive(false);
-        GetImage((int)Images.ScoreImage_3).gameObject.SetActive(false);
-        GetImage((int)Images.ScoreImage_4).gameObject.SetActive(false);
-        GetImage((int)Images.ScoreImage_5).gameObject.SetActive(false);
+        for (int i = 0; i < _player.PlayerInfo.CardDecks.Count; i++)
+        {
+            GetText((int)Texts.ScoreText_1 + i).transform.parent.gameObject.SetActive(false);
+        }
 
         Refresh();
 
@@ -134,27 +127,22 @@ public class UI_GameplayScene : UI_Scene
 
     private void Refresh()
     {
-        // 골드 텍스트
+        // 플레이어의 골드 정보 업데이트
         GetText((int)Texts.GoldText).text = _player.PlayerInfo.Gold.ToString();
-        // 배팅 텍스트
-        GetText((int)Texts.BetText_1).text = _player.PlayerInfo.CardDecks[0].Bet.ToString();
-        GetText((int)Texts.BetText_2).text = _player.PlayerInfo.CardDecks[1].Bet.ToString();
-        GetText((int)Texts.BetText_3).text = _player.PlayerInfo.CardDecks[2].Bet.ToString();
-        GetText((int)Texts.BetText_4).text = _player.PlayerInfo.CardDecks[3].Bet.ToString();
-        GetText((int)Texts.BetText_5).text = _player.PlayerInfo.CardDecks[4].Bet.ToString();
-        // 점수 텍스트
-        GetText((int)Texts.ScoreText_1).text = _player.PlayerInfo.CardDecks[0].Score.ToString();
-        GetText((int)Texts.ScoreText_2).text = _player.PlayerInfo.CardDecks[1].Score.ToString();
-        GetText((int)Texts.ScoreText_3).text = _player.PlayerInfo.CardDecks[2].Score.ToString();
-        GetText((int)Texts.ScoreText_4).text = _player.PlayerInfo.CardDecks[3].Score.ToString();
-        GetText((int)Texts.ScoreText_5).text = _player.PlayerInfo.CardDecks[4].Score.ToString();
+        // 플레이어의 모든 카드 덱을 순회하며 각 덱의 배팅 금액과 점수를 업데이트
+        for (int i = 0; i < _player.PlayerInfo.CardDecks.Count; i++) 
+        {
+            GetText((int)Texts.BetText_1 + i).text = _player.PlayerInfo.CardDecks[i].Bet.ToString();
+            GetText((int)Texts.ScoreText_1 + i).text = _player.PlayerInfo.CardDecks[i].Score.ToString();
+        }
+        // Dealer 점수 업데이트
         GetText((int)Texts.DealerScoreText).text = _dealer.Score.ToString();
-        // Chip 버튼
+        // 각 Chip 버튼의 활성 상태를 업데이트
         GetButton((int)Buttons.Chip10Button).interactable = _player.PlayerInfo.Gold >= (int)Chip.Chip10;
         GetButton((int)Buttons.Chip50Button).interactable = _player.PlayerInfo.Gold >= (int)Chip.Chip50;
         GetButton((int)Buttons.Chip100Button).interactable = _player.PlayerInfo.Gold >= (int)Chip.Chip100;
         GetButton((int)Buttons.Chip500Button).interactable = _player.PlayerInfo.Gold >= (int)Chip.Chip500;
-        // Play 버튼
+        // Play 버튼의 활성 상태를 업데이트
         GetButton((int)Buttons.PlayButton).interactable = _player.PlayerInfo.TotalBet > 0;
     }
 
@@ -199,12 +187,19 @@ public class UI_GameplayScene : UI_Scene
         {
             GetObejct((int)GameObjects.SetBetting).SetActive(false);
             GetObejct((int)GameObjects.SetPlaying).gameObject.SetActive(true);
-            GetImage((int)Images.ScoreImage_1).gameObject.SetActive(true);
-            GetImage((int)Images.ScoreImage_2).gameObject.SetActive(true);
-            GetImage((int)Images.ScoreImage_3).gameObject.SetActive(true);
-            GetImage((int)Images.ScoreImage_4).gameObject.SetActive(true);
-            GetImage((int)Images.ScoreImage_5).gameObject.SetActive(true);
+            // 플레이어의 모든 카드 덱을 순회하며 해당 인덱스에 맞는 토글, BetImage, ScoreImage의 활성화 상태 설정
+            for (int i = 0; i < _player.PlayerInfo.CardDecks.Count; i++)
+            {
+                bool isBet = _player.PlayerInfo.CardDecks[i].Bet > 0;
+                GetToggle((int)Toggles.CardDeckToggle_1 + i).interactable = isBet;
+                GetText((int)Texts.BetText_1 + i).transform.parent.gameObject.SetActive(isBet);
+                GetText((int)Texts.ScoreText_1 + i).transform.parent.gameObject.SetActive(isBet);
+            }
+
+            _player.Play();
         }
+
+        Refresh();
     }
 
     private void OnClickHitButton()
